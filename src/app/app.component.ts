393ed19cd5b7dfbb40b1c86f5208aa9dff1abb8b
Raj Singh <rajs@maveric-systems.com>
2023-12-18 22:33:24 +0530

    'form'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Directive, ElementRef, HostListener, Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Directive({
+  selector: "img[appHideMissing]",
 })
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+
 export class AppComponent {
   title = 'angularForm';
+  constructor(private el: ElementRef) {}
+
+  @HostListener("error")
+  private onError() {
+    this.el.nativeElement.style.display = "none";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    routingComponent,
+    AppComponent,
+    // LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
